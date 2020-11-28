b7b35f25fe3d70f7cda08ad1d47eca126df59f43
Orestis Lomis <orestis.lomis@student.kuleuven.be>
2020-11-28 18:12:30 +0100

    made make_level() a bit cleaner

--- main/main.py
+++ main/main.py
@@ -1,7 +1,7 @@
 import random
 
 def make_level(colors):
-    all_states = [[] for i in range(colors)]
+    all_states = [[] for i in range(colors + 2)]
     print(all_states)
     for color in range(colors):
         balls = 0
@@ -10,9 +10,6 @@ def make_level(colors):
             if len(all_states[tube]) < 4:
                 all_states[tube].append(color)
                 balls += 1
-    print(all_states)
-    all_states.append([])
-    all_states.append([])
     print(all_states)
 
 make_level(7)
