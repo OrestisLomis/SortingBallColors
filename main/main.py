d834071b83e2c6976a1e2fc2fcf3601df39002c4
Orestis Lomis <orestis.lomis@student.kuleuven.be>
2020-12-01 15:07:25 +0100

    loop-breaking algorithm finished and finds a solution for level 9 after a long time due to random search

--- main/main.py
+++ main/main.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 def make_level(colors):
     assert colors >= 2
@@ -17,10 +18,9 @@ def make_level(colors):
 def move_ball(tubes, start, goal):
     # assert len(tubes[start]) > 0
     # assert len(tubes[goal]) < 4
-    print(start, goal)
-    if can_move(tubes, start, goal):
-        ball = tubes[start].pop(-1)
-        tubes[goal].append(ball)
+    # print(start, goal)
+    ball = tubes[start].pop(-1)
+    tubes[goal].append(ball)
     return tubes
 
 def can_move_physically(tubes, start, goal):
@@ -29,24 +29,31 @@ def can_move_physically(tubes, start, goal):
 def can_move_legally(tubes, start, goal):
     return len(tubes[goal]) == 0 or tubes[start][-1] == tubes[goal][-1]
 
-def can_move(tubes, start, goal):
-    return can_move_physically(tubes, start, goal) and can_move_legally(tubes, start, goal)
+def visited(tubes, states):
+    return tubes in states
 
-def get_all_moves_from_tube(tubes, start):
+def can_move(tubes, start, goal, states):
+    tubes_copy = copy.deepcopy(tubes)
+    if can_move_physically(tubes, start, goal) and can_move_legally(tubes, start, goal):
+        tubes_copy = move_ball(tubes_copy, start, goal)
+        return not visited(sorted(tubes_copy), states)
+    return False
+
+def get_all_moves_from_tube(tubes, start, states):
     all_moves = []
     for goal in range(len(tubes)):
-        if can_move(tubes, start, goal):
+        if can_move(tubes, start, goal, states):
             all_moves.append((start, goal))
     return all_moves
 
-def get_all_moves(tubes):
+def get_all_moves(tubes, states):
     all_moves = []
     for start in range(len(tubes)):
-        all_moves.extend(get_all_moves_from_tube(tubes, start))
+        all_moves.extend(get_all_moves_from_tube(tubes, start, states))
     return all_moves
 
-def game_over(tubes):
-    return len(get_all_moves(tubes)) == 0
+def game_over(tubes, states):
+    return len(get_all_moves(tubes, states)) == 0
 
 def complete(tube):
     if len(tube) == 0:
@@ -68,20 +75,31 @@ def end_state(tubes):
     if win(tubes):
         print('WIN!')
         return True
-    if game_over(tubes):
+    if game_over(tubes, states):
         print('GAME OVER!')
         return True
     return False
 
 
-states = make_level(6)
-for i in range(10000):
-    all_moves = get_all_moves(states)
-    print(all_moves)
-    move = all_moves[random.randint(0, len(all_moves) - 1)]
-    states = move_ball(states, move[0], move[1])
-    if end_state(states):
-        break
-    print(states)
+tubes = make_level(9)
+tubes_orig = copy.deepcopy(tubes)
+states = list()
+while not win(tubes):
+    tubes = copy.deepcopy(tubes_orig)
+    states = []
+    while not end_state(tubes):
+        tubes_copy = copy.deepcopy(tubes)
+        tubes_copy = sorted(tubes_copy)
+        
+        all_moves = get_all_moves(tubes, states)
+        print(all_moves)
+        move = all_moves[random.randint(0, len(all_moves) - 1)]
+        print(move)
+        tubes = move_ball(tubes, move[0], move[1])
+        print(tubes) 
+        states.append(tubes_copy)
+    
+    
+    
 
 
